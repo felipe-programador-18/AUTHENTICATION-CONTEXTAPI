67ae1ff98bdd6d794561ac6af3d22026c792280b
Felipe <machadofelipe2016@outlook.com>
2022-04-26 18:13:52 -0300

    c42-called name of button

--- autenticacao/src/Comment.js
+++ autenticacao/src/Comment.js
@@ -2,13 +2,11 @@ import React, {useContext}  from 'react'
 import Time from './Time'
 import { AuthContext } from './auth'
 
-
-
 //created comment to turn on with comments and time and auth // 
 const Comment = ({comment}) => {
   const auth = useContext(AuthContext)
     return(<div>
-     {comment.content} por: {comment.users.name} em <Time TIMESTAMP={comment.CreateAT}/>
+     {comment.content} por: {comment.users.name} em <Time TIMESTAMP={comment.createAT}/>
     </div>)
 }
 
