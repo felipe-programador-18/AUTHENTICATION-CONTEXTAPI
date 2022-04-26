7d9d09b4ce13dab553ddbb54586dc6313edb10d2
Felipe <machadofelipe2016@outlook.com>
2022-04-26 15:49:16 -0300

    c20-adding authprovider about app

--- autenticacao/src/App.js
+++ autenticacao/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import {AuthProvider} from './auth'
 import './App.css';
 
 
 function App() {
   return (
+    <AuthProvider >
     <div className="App">
      
     </div>
+
+    </AuthProvider>
   );
 }
 
