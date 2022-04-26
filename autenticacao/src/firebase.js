fc8b9de1ab4cbe9eec0326bb7844613820c4b855
Felipe <machadofelipe2016@outlook.com>
2022-04-26 13:47:52 -0300

    c9-adding date about firebase to connect him

--- autenticacao/src/firebase.js
+++ autenticacao/src/firebase.js
@@ -3,6 +3,17 @@ import firebase from 'firebase/compat/app'
 import 'firebase/compat/database' 
 import 'firebase/compat/auth'
 
+const firebaseConfig = {
+    apiKey: "AIzaSyAjCtynXfx7QTLoaZCYb3KYBD_9PlTzAVE",
+    authDomain: "autenticacao-pratica.firebaseapp.com",
+    projectId: "autenticacao-pratica",
+    storageBucket: "autenticacao-pratica.appspot.com",
+    messagingSenderId: "270390894713",
+    appId: "1:270390894713:web:b45e791ed19988a53e34a8"
+  };
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-
+export default firebase
 
