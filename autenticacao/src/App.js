bd2a1428d70a8896df7dcd3ce0e79b37e9d78fbd
Felipe <machadofelipe2016@outlook.com>
2022-04-26 17:08:55 -0300

    c30-adding createuser in app

--- autenticacao/src/App.js
+++ autenticacao/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import {AuthProvider} from './auth'
 import Comments from './Comments'
-import './App.css';
+
+import NewComments from './Newcomments'
+import './App.css'
+import CreateUser from './CreateUser'
 
 
 function App() {
   return (
     <AuthProvider >
     <div className="App">
+     <NewComments/> 
      <Comments/>
+     <CreateUser/>
+     
     </div>
 
     </AuthProvider>
