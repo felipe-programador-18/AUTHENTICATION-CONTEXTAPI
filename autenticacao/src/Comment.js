afe25942bf13c7fd3a59f60ce815ef3dfe8a8b18
Felipe <machadofelipe2016@outlook.com>
2022-04-26 14:21:05 -0300

    c12-create all file database and create two function

---
+++ autenticacao/src/Comment.js
@@ -0,0 +1,33 @@
+import  {useState, useEffect} from 'react'
+import firebase from './firebase'
+
+//remember this is connect all date to adding new date in database
+
+export const UsedatabasePush = endpoints => {
+ const [status,setstatus] = useState('')
+  const save = data => {
+     const ref = firebase.database().ref(endpoints)
+     ref.push(data,err => {
+         if(err){
+             setstatus('Error in application')
+         }else{
+             setstatus('Success')
+         }
+     })
+   }
+    return [status, save]
+}
+
+export const useDatabase = endpoints =>{
+  const [data, setdata] = useState({})
+  useEffect(() => {
+    const ref = firebase.database().ref(endpoints)
+    ref.on('value', snapshoot => {
+        setdata(snapshoot.val())
+    })
+   return () => {
+       ref.off()
+   }
+  }, [endpoints])
+   return data
+}
