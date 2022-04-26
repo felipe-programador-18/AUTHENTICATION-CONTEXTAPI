a5f08467b304a59be6087fe7bf1ad855e6d4a5ba
Felipe <machadofelipe2016@outlook.com>
2022-04-26 18:11:25 -0300

    c38- rearrange all code

--- autenticacao/src/App.js
+++ autenticacao/src/App.js
@@ -5,6 +5,7 @@ import NewComments from './Newcomments'
 import './App.css'
 import CreateUser from './CreateUser'
 import UserInfo from './UserInfo'
+import SingInUser from './SingInUser'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
      <NewComments/> 
      <Comments/>
      <CreateUser/>
+     <SingInUser/>
      <UserInfo/>
     </div>
 
